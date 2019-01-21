a763b80f4bd2f4fcadd1146c611cecc9ef636a78
CodeLionX <sebastian.schmidl@t-online.de>
2019-01-21 11:36:38 +0100

    add argument parser

--- build.sbt
+++ build.sbt
@@ -12,17 +12,24 @@ organization := "com.github.codelionx"
 ThisBuild / scalaVersion := "2.12.7"
 
 val flinkVersion = "1.7.1"
-
-val flinkDependencies = Seq(
-  "org.apache.flink" %% "flink-scala" % flinkVersion % "provided",
-  "org.apache.flink" %% "flink-streaming-scala" % flinkVersion % "provided")
+val clistVersion = "3.5.0"
 
 lazy val root = (project in file(".")).
   settings(
-    libraryDependencies ++= flinkDependencies,
-    libraryDependencies += "org.apache.logging.log4j" % "log4j-slf4j18-impl" % "2.11.1",
-    libraryDependencies += "org.apache.logging.log4j" % "log4j-api" % "2.11.1",
-    libraryDependencies += "org.apache.logging.log4j" % "log4j-core" % "2.11.1"
+    libraryDependencies ++= Seq(
+      "org.apache.flink" %% "flink-scala" % flinkVersion % "provided",
+      "org.apache.flink" %% "flink-streaming-scala" % flinkVersion % "provided"
+    ),
+    libraryDependencies ++= Seq(
+      // logging
+      "org.apache.logging.log4j" % "log4j-slf4j18-impl" % "2.11.1",
+      "org.apache.logging.log4j" % "log4j-api" % "2.11.1",
+      "org.apache.logging.log4j" % "log4j-core" % "2.11.1",
+      // cli parser
+      "org.backuity.clist" %% "clist-core" % clistVersion,
+      "org.backuity.clist" %% "clist-macros" % clistVersion % "provided",
+    )
+
 )
 
 assembly / mainClass := Some("com.github.codelionx.AnalyzeHTTPLog")
--- src/main/scala/com/github/codelionx/AnalyzeHTTPLog.scala
+++ src/main/scala/com/github/codelionx/AnalyzeHTTPLog.scala
@@ -1,19 +1,46 @@
 package com.github.codelionx
 
-import org.apache.flink.api.scala._
+import java.io.File
+
+import org.apache.flink.api.common.state.StateTtlConfig.TimeCharacteristic
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.backuity.clist
+import org.backuity.clist.CliMain
 
 
 /**
  * Skeleton for our Flink Job.
  *
  */
-object AnalyzeHTTPLog {
-  def main(args: Array[String]) {
-    // set up the execution environment
-    val env = ExecutionEnvironment.getExecutionEnvironment
+object AnalyzeHTTPLog extends CliMain[Unit](
+  name = "AnalyzeHTTPLog",
+  description = "Flink job for analyzing NASAs HTTP log"
+) {
 
+  var path = clist.opt[String](description = "specify path to the log file to analyze", default = "./access_log_Aug95")
+  var cores = clist.opt[Int](description = "number of cores / parallelism", default = 4)
+
+  override def run: Unit = {
+    val env = setupEnvironment()
+
+    val httpTextLog = getLogStream(env)
+    httpTextLog.print()
 
     // execute program
     env.execute("Flink Scala API Skeleton")
+  }
+
+  private def setupEnvironment(): StreamExecutionEnvironment = {
+    // set up the execution environment
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    // define parallelism
+    env.setMaxParallelism(cores)
+    env.setParallelism(cores)
+
+    env
+  }
+
+  private def getLogStream(env: StreamExecutionEnvironment): DataStream[String] = {
+    env.readTextFile(path, "ASCII")
   }
 }
