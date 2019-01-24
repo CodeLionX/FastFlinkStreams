b86cdc83fac5255fb5c74d30f7198d26ccfd09ce
CodeLionX <sebastian.schmidl@t-online.de>
2019-01-24 15:19:14 +0100

    use side output for corrupted log lines

--- src/main/scala/com/github/codelionx/AnalyzeHTTPLog.scala
+++ src/main/scala/com/github/codelionx/AnalyzeHTTPLog.scala
@@ -3,14 +3,12 @@ package com.github.codelionx
 import java.time._
 import java.time.format.DateTimeFormatter
 
-import org.apache.flink.api.common.accumulators.AverageAccumulator
 import org.apache.flink.api.common.functions.AggregateFunction
 import org.apache.flink.api.scala._
 import org.apache.flink.core.fs.FileSystem.WriteMode
 import org.apache.flink.streaming.api.TimeCharacteristic
-import org.apache.flink.streaming.api.functions.sink.SinkFunction
-import org.apache.flink.streaming.api.scala.DataStreamUtils
-import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.flink.streaming.api.functions.ProcessFunction
+import org.apache.flink.streaming.api.scala.{DataStream, OutputTag, StreamExecutionEnvironment}
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.util.Collector
@@ -18,7 +16,6 @@ import org.backuity.clist
 import org.backuity.clist.CliMain
 import org.slf4j.LoggerFactory
 
-import scala.collection.mutable
 import scala.util.{Failure, Success, Try}
 
 
@@ -66,6 +63,8 @@ object HTTPLogEntry {
   }
 }
 case class HTTPLogEntry(host: String, date: ZonedDateTime, httpMethod: String, resource: String, responseCode: Int, responseBytes: Int)
+case class PressureAlert(host: String, date: ZonedDateTime, nRequests: Int)
+case class CorruptedLog(logline: String, error: Throwable)
 
 
 /**
@@ -84,11 +83,15 @@ object AnalyzeHTTPLog extends CliMain[Unit](
   override def run: Unit = {
     val env = setupEnvironment()
 
-    val httpTextLog = getLogStream(env)
+    val streamTuple = getLogStream(env)
+    val httpTextLog = streamTuple._1
+    val corruptedLog = streamTuple._2
 
     val requestsPerDayFile = "requestsPerDay"
     val uniqueVisitorsFile = "uniqueVisitors"
     val serverErrorsFile = "serverErrors"
+    val suspiciousHostsFile = "suspiciousHostsFile"
+    val corruptedLogsFile = "corruptedLogs"
     averageRequestsPerDay(httpTextLog)
       .writeAsCsv(requestsPerDayFile, WriteMode.OVERWRITE)
       .setParallelism(1)
@@ -98,28 +101,33 @@ object AnalyzeHTTPLog extends CliMain[Unit](
     serverErrorsPerMonth(httpTextLog)
       .writeAsCsv(serverErrorsFile, WriteMode.OVERWRITE)
       .setParallelism(1)
+    suspiciousHosts(httpTextLog)
+        .map( alert => (alert.host, alert.date, alert.nRequests))
+        .writeAsCsv(suspiciousHostsFile, WriteMode.OVERWRITE)
+        .setParallelism(1)
+    corruptedLog
+        .map( event => (event.logline, event.error))
+        .writeAsCsv(corruptedLogsFile, WriteMode.OVERWRITE)
+        .setParallelism(1)
 
     // execute program
     env.execute("HTTP log analysis")
 
     // collect output measurements
-    //    val requestsPerDayCol = new DataStreamUtils(requestsPerDay).collect().toSeq
     val requestsPerDayCol = io.Source.fromFile(requestsPerDayFile).getLines().map( _.split(",")(1).toLong)
     val result = requestsPerDayCol
       .map( requests => 1 -> requests )
       .reduce( (t1, t2) =>
         (t1._1 + t2._1) -> (t1._2 + t2._2)
       )
-    println(s"Avg. requests per day: ${result._2 / result._1}")
-//
-//    val uniqueVisitorsADayCol = new DataStreamUtils(uniqueVisitorsStream).collect().toSeq
+    println(s"Avg. requests per day : ${result._2 / result._1}")
+
     val uniqueVisitorsADayCol = io.Source.fromFile(uniqueVisitorsFile).getLines().map(_.toLong).toSeq
     val minUnqiueVisitors = uniqueVisitorsADayCol.min
     val maxUniqueVisitors = uniqueVisitorsADayCol.max
-    println(s"Min. unique visitors a day: $minUnqiueVisitors")
-    println(s"Max. unique visitors a day: $maxUniqueVisitors")
-//
-//    val serverErrorsCol = new DataStreamUtils(serverErrors).collect().toSeq
+    println(s"Min. unique visitors a day : $minUnqiueVisitors")
+    println(s"Max. unique visitors a day : $maxUniqueVisitors")
+
     val serverErrorsCol = io.Source.fromFile(serverErrorsFile).getLines().map( line => {
       val values = line.split(",")
       values(0) -> values(1).toInt
@@ -127,11 +135,18 @@ object AnalyzeHTTPLog extends CliMain[Unit](
     serverErrorsCol.foreach{ case (descr, errors) =>
       println(s"Server errors $descr : $errors")
     }
+
+    val suspiciousHostsCol = io.Source.fromFile(suspiciousHostsFile).getLines().map(_.split(",")(0)).toSet
+    println(s"Hosts that performed more than 10 requests a second : ${suspiciousHostsCol.mkString(", ")}")
+
+    // collect and count corrupted logs
+    val nCorruptedLogs = io.Source.fromFile(corruptedLogsFile).getLines().count(_ => true)
+    println(s"Corrupted log lines : $nCorruptedLogs")
   }
 
   private def averageRequestsPerDay(httpTextLog: DataStream[HTTPLogEntry]): DataStream[(LocalDate, Int)] = {
     // count all requests per day
-    val requestsPerDay = httpTextLog
+    httpTextLog
       .timeWindowAll(Time.days(1))
       .apply( (window, events, out: Collector[(LocalDate, Int)]) =>
         out.collect((
@@ -139,12 +154,6 @@ object AnalyzeHTTPLog extends CliMain[Unit](
           events.count(_ => true)
         ))
       )
-
-//    requestsPerDay
-//      .map(t => s"Requests/day for day ${t._1} : ${t._2}")
-//      .print()
-
-    requestsPerDay
   }
 
   private def uniqueVisitors(logStream: DataStream[HTTPLogEntry]): DataStream[Long] = {
@@ -171,6 +180,18 @@ object AnalyzeHTTPLog extends CliMain[Unit](
       })
   }
 
+  private def suspiciousHosts(logStream: DataStream[HTTPLogEntry]): DataStream[PressureAlert] = {
+    logStream
+      .keyBy(_.host)
+      .timeWindow(Time.seconds(1))
+      .apply( (key, window, events, out: Collector[PressureAlert]) => {
+        val n = events.count( _ => true)
+        if(n >= 10) {
+          out.collect(PressureAlert(key, ZonedDateTime.ofInstant(Instant.ofEpochMilli(window.getStart), ZoneId.of("UTC")), n))
+        }
+      })
+  }
+
   private def setupEnvironment(): StreamExecutionEnvironment = {
     // set up the execution environment
     val env = StreamExecutionEnvironment.getExecutionEnvironment
@@ -184,14 +205,24 @@ object AnalyzeHTTPLog extends CliMain[Unit](
     env
   }
 
-  private def getLogStream(env: StreamExecutionEnvironment): DataStream[HTTPLogEntry] = {
+  private def getLogStream(env: StreamExecutionEnvironment): (DataStream[HTTPLogEntry], DataStream[CorruptedLog]) = {
     val textStream = env.readTextFile(path, "UTF-8")
-    val entryStream = textStream.flatMap( (logLine: String, collector: Collector[HTTPLogEntry]) => {
-      HTTPLogEntry.fromString(logLine) match {
-        case Success(logEntry) => collector.collect(logEntry)
-        case Failure(e) => log.warn(s"Ignoring log entry $logLine", e)
+    val errorLogTag = OutputTag[CorruptedLog]("corruptedlogs")
+//    val entryStream = textStream.flatMap( (logLine: String, collector: Collector[HTTPLogEntry]) => {
+//      HTTPLogEntry.fromString(logLine) match {
+//        case Success(logEntry) => collector.collect(logEntry)
+//        case Failure(e) => log.warn(s"Ignoring log entry $logLine", e)
+//      }
+//    })
+    val entryStream = textStream.process[HTTPLogEntry]( (value: String, ctx: ProcessFunction[String, HTTPLogEntry]#Context, out: Collector[HTTPLogEntry]) => {
+      HTTPLogEntry.fromString(value) match {
+        case Success(logEntry) => out.collect(logEntry)
+        case Failure(e) => ctx.output(errorLogTag, CorruptedLog(value, e))
       }
     })
-    entryStream.assignAscendingTimestamps( logEntry => logEntry.date.toInstant.toEpochMilli )
+    (
+      entryStream.assignAscendingTimestamps( logEntry => logEntry.date.toInstant.toEpochMilli ),
+      entryStream.getSideOutput(errorLogTag)
+    )
   }
 }
